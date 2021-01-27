262d672de43fdda558b005d774789f467b138e0f
edgarjt <edgarjt97@gmail.com>
2021-01-27 03:13:15 +0000

    update controller service

--- app/Http/Controllers/ResourcesController.php
+++ app/Http/Controllers/ResourcesController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\InfoWeb;
 
 class ResourcesController extends Controller
 {
     public function contacto(Request $request) {
+        $inf = InfoWeb::where('inf_id', 1)->first();
+        $mail = $inf->inf_email;
         //validar json
-        if ($request->isJson()) {
+        if ($request->isJson() && $mail) {
             ini_set( 'display_errors', 1 );
             error_reporting( E_ALL );
             $from = $request->email;
-            $to = "edgarjt97@gmail.com";
+            $to = $mail;
             $subject = "Mensaje de contacto Yeguada San Rafael";
             $message = "Nombre: $request->name \n";
             $message .= "Mensaje: $request->message";
